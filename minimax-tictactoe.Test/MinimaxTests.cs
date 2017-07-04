da67563d44792355b5235c9aa2c1452e0259f7e8
Ran, Ido <ido.ran@hpe.com>
2017-07-04 11:42:59 +0300

    Add testing project with two example test methods

---
+++ minimax-tictactoe.Test/MinimaxTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace minimax_tictactoe.Test
+{
+    [TestClass]
+    public class MinimaxTests
+    {
+        [TestMethod]
+        public void WhenOnlyOneCellIsOpenAlgChooseThatCell()
+        {
+            int result = 1 + 2;
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestThatFail()
+        {
+            int result = 1 + 3;
+            Assert.AreEqual(19, result);
+        }
+    }
+}
---
+++ minimax-tictactoe.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("minimax-tictactoe.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("minimax-tictactoe.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9902f6f4-046c-4215-809d-5790f0faff79")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
